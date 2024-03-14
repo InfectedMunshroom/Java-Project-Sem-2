756f06b07d9d1f793359e844bd0b944bd6db9f8f
InfectedMunshroom <hingoraniayushmanagrawal@gmail.com>
2024-03-14 06:40:10 +0530

    new file:   ClientSide.class 	modified:   ClientSide.java 	new file:   ClientSideMultiple.class 	new file:   ClientSideMultiple.java 	new file:   ServerSide.class 	modified:   ServerSide.java

--- ClientSide.java
+++ ClientSide.java
@@ -1,24 +1,66 @@
+// import java.net.*;
+// import java.io.*;
+// import java.util.Scanner;
+
+// class ClientSide{
+//     public static void main(String[] args){
+//         Socket cSocket;
+//         Scanner in = new Scanner(System.in);
+//         try{
+//             cSocket = new Socket("localhost",5005);
+//             System.out.println("Message: ");
+//             String alpha = in.nextLine();
+//             DataOutputStream dout = new DataOutputStream(cSocket.getOutputStream());
+//             dout.writeUTF(alpha);
+//             dout.flush();
+//             dout.close();
+//             cSocket.close();
+//             in.close();
+//         }
+//         catch(Exception e){
+//             System.out.println("[-] Could not send message");
+//         }
+//     }
+// }
+
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-class ClientSide{
-    public static void main(String[] args){
-        Socket cSocket;
-        Scanner in = new Scanner(System.in);
+public class ClientSide{
+    private Socket socket;
+    private DataOutputStream dout;
+    private Scanner in;
+
+    public ClientSide(){
         try{
-            cSocket = new Socket("localhost",5005);
-            System.out.println("Message: ");
-            String alpha = in.nextLine();
-            DataOutputStream dout = new DataOutputStream(cSocket.getOutputStream());
-            dout.writeUTF(alpha);
-            dout.flush();
-            dout.close();
-            cSocket.close();
-            in.close();
+            socket = new Socket("localhost",ServerSide.port);
+            dout = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Your message: ");
+            in = new Scanner(System.in);
+            writeMessage();
+
         }
         catch(Exception e){
-            System.out.println("[-] Could not send message");
+            System.out.println("[-] Error creating client");
         }
+    }
+    private void writeMessage() throws IOException{
+        String line = "";
+        while(!line.equals(ServerSide.STOP_STRING)){
+            line = in.nextLine();
+            dout.writeUTF(line);
+        }
+        close();
+
+    }
+    private void close() throws IOException{
+        socket.close();
+        in.close();
+        dout.close();
+    }
+
+    public static void main(String[] args){
+        new ClientSide();
     }
 }
--- ServerSide.java
+++ ServerSide.java
@@ -1,31 +1,76 @@
+// import java.net.*;
+// import java.io.*;
+
+// class ServerSide{
+//     public static void main(String[] args){
+//         ServerSocket sSocket;
+//         Socket cSocket;
+//         try{
+//             sSocket = new ServerSocket(5005);
+//             cSocket = sSocket.accept();
+//             try{
+//                 DataInputStream dis = new DataInputStream(cSocket.getInputStream());
+//                 String str = (String)dis.readUTF();
+//                 System.out.println(str);
+                
+//             }
+//             catch(Exception e){
+//                 System.out.println("[-] Message could not be read");
+//             }
+//             sSocket.close();
+//         }
+
+//         catch(Exception e){
+//             System.out.println("[-] Server connected to port");
+//         }
+
+
+
+
+//     }
+// }
+
+
+
 import java.net.*;
 import java.io.*;
 
 class ServerSide{
-    public static void main(String[] args){
-        ServerSocket sSocket;
-        Socket cSocket;
+    public static final int port = 5555;
+    private ServerSocket server;
+    public static final String STOP_STRING = "--++";
+
+    public ServerSide(){
         try{
-            sSocket = new ServerSocket(5005);
-            cSocket = sSocket.accept();
-            try{
-                DataInputStream dis = new DataInputStream(cSocket.getInputStream());
-                String str = (String)dis.readUTF();
-                System.out.println(str);
-                
-            }
-            catch(Exception e){
-                System.out.println("[-] Message could not be read");
-            }
-            sSocket.close();
+            server = new ServerSocket(port);
+            System.out.println("[+] Server Successfully Online");
+            while(true) clientSideConnections();
         }
-
         catch(Exception e){
-            System.out.println("[-] Server connected to port");
+            System.out.println("[-] Error: Check Server");
         }
-
-
-
-
     }
+
+    private void clientSideConnections(){
+        try{
+            Socket cSocket = server.accept();
+            if(cSocket.isConnected()){
+            new Thread(()->{
+                ClientSideMultiple client = new ClientSideMultiple(cSocket);
+                client.readMessages();
+                client.close();
+
+        }).start();}
+        }
+        catch(Exception e){
+            System.out.println("[-] Failure in Connection of Client");
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        ServerSide Server = new ServerSide();
+    }
+
 }
