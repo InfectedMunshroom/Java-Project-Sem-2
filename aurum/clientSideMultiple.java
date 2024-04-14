8fc6d818d0ee981fb8b73b17b4870fc58630e129
InfectedMunshroom <hingoraniayushmanagrawal@gmail.com>
2024-04-14 14:34:52 +0530

    Changes to be committed: 	modified:   aurum/clientSideMultiple.java

--- aurum/clientSideMultiple.java
+++ aurum/clientSideMultiple.java
@@ -1,16 +1,49 @@
 package aurum;
+
 import java.io.*;
 import java.net.*;
-import java.sql.ClientInfoStatus;
+import java.util.*;
 
+public class clientSideMultiple{
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
 
-public class clientSideMultiple {
-    private Socket clientSocket;
-    private DataInputStream dis;
-    private DataOutputStream dout;
+    public clientSideMultiple(Socket socket, String username){
+        try{
+            this.socket = socket;
+            this.username = username;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        }
+        catch(Exception e){
+            System.out.println("Error in creating client");
+            closeEverything(socket,bufferedReader,bufferedWriter);
+        }
 
-    public clientSideMultiple(){
+    }
+
+    public void sendMessage(){
+        try{
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+            Scanner sc = new Scanner(System.in);
+            while(socket.isConnected()){
+                String messageToSend = sc.nextLine();
+                bufferedWriter.write(username+": "+messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        }
+        catch(Exception e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void listenForMessage(){
         
     }
-    
+
 }
